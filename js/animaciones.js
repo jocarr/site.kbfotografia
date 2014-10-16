8cb35a36997696d7b467c6dda8e23403e2c21d4d
jocarr <josemanuel.carrillo@me.com>
2014-10-16 00:29:13 -0600

    Contacto y galeria bodas
    Paginas creadas:
    -Contacto
    -Galeria Bodas

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,51 +1,44 @@
 $(document).ready(function(){
 
-$(".cover-heading").mouseenter(function(){velocity({ 
-	backgroudColor : '#fff'
-	}, {
-		duration : 500
-	})
-});
-/*var InfiniteRotator = {
-		
-	init: function() {
-		var initialFadeIn = 1000;
+	$('.scroll').click(function() {
 
-		var itemInterval = 5000;
+		var clickedURL = $(this).attr('href');
 
-		var fadeTime =2500;
+		$(clickedURL).velocity("scroll", {duration: 1000, easing: "InOutCubic", offset: -50});
+	});
 
-		var numberOfItems = $('.rotating-item').length;
+	$('.fa').hover(function() {
 
-		var currentItem = 0;
+		var sn = '#' + $(this).attr('id');
+		var actColor = $(this).css('color');
 
-		var backgrouds = ['url(../../img/portada)', 'url(../../img/portada1)', 'url(../../img/portada2)', 'url(../../img/portada3)', 'url(../../img/portada4)' ];
+		switch (sn) {
+			case '#facebook':
+				$(sn).velocity({color: '#3B5998'}, {duration: 200});
+				break;
 
-		$('rotating-item').eq(currentItem).velocity("fadeIn", {duration : initialFadeIn});
+			case '#twitter':
+				$(sn).velocity({color: '#4099FF'}, {duration: 200});
+				break;
 
-		var infiniteLoop = setInterval(function () {
-			$('rotating-item').eq(currentItem).velocity("fadeOut",{duration : fadeTime});
+			case '#instagram':
+				$(sn).velocity({color: '#675144'}, {duration: 200});
+				break;
 
-			if (currentItem == numberOfItems -1) {
-				currentItem = 0;
-			}else{
-				currentItem++;
-			}
-			$('.rotating-item').eq(currentItem).velocity("fadeIn",{duration : fadeTime});
-		}, itemInterval);
-		
-		// $('body').velocity("fadeIn", {duration :})
-	}	
-};
-*/
-//InfiniteRotator.init();
+			case '#pinterest':
+				$(sn).velocity({color: '#CB2027'}, {duration: 200});
+				break;
+		}
+	});
 
-// Scroll
-
-	$('#down').click(function() {
-		$('#frase1').velocity("scroll", {duration: 1000, easing: "InOutCubic"});
+	$('.fa').mouseout(function() {
+		$(this).velocity({color: '#9c9c9c'}, {duration: 400});
 	});
   });
+
+
+
+	
 
 
 
