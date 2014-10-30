ac9fb6822e37ade6f51c6685d7736747143d1a22
jocarr <josemanuel.carrillo@me.com>
2014-10-29 21:53:50 -0600

    Portada Completada

--- js/animaciones.js
+++ js/animaciones.js
@@ -15,19 +15,27 @@ $(document).ready(function(){
 ---------- cambia color en Hover ------------
 =============================================
 */
+	var Color = 0;
 
-	$('.fa').hover(function() {
+	function rgb2hex(rgb){
+		 rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+		 return (rgb && rgb.length === 4) ? "#" +
+		  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+		  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+		  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+	}	
 
+	$('.fa').mouseenter(function() {
+		this.Color = rgb2hex($(this).css('color'));
 		var sn = '#' + $(this).attr('id');
-		var actColor = $(this).css('color');
-
+		
 		switch (sn) {
 			case '#facebook':
 				$(sn).velocity({color: '#3B5998'}, {duration: 200});
 				break;
 
 			case '#twitter':
-				$(sn).velocity({color: '#4099FF'}, {duratio26632039n: 200});
+				$(sn).velocity({color: '#4099FF'}, {duration: 200});
 				break;
 
 			case '#instagram':
@@ -40,15 +48,16 @@ $(document).ready(function(){
 		}
 	});
 
-	$('.fa').mouseout(function() {
-		$(this).velocity({color: '#9c9c9c'}, {duration: 400});
+	$('.fa').mouseleave(function() {
+		$(this).velocity({color: this.Color}, {duration: 0});
+		console.log(this.Color);
 	});
 
-	$('.cover-heading').hover(function() {
-		$('#frame-logo').velocity({backgroundColor : '#000', backgroundColorAlpha: 0.5}, {duration: 500});
+	$('.cover-heading').mouseenter(function() {
+		$('#frame-logo').velocity({ backgroundColorAlpha: 0.5}, {duration: 500});
 	});
 
-	$('.cover-heading').mouseout(function() {
+	$('.cover-heading').mouseleave(function() {
 		$('#frame-logo').velocity({backgroundColorAlpha: 0}, {duration: 400});
 	});
 
@@ -72,8 +81,31 @@ $(document).ready(function(){
 		paralax();
 
 	});
+
+
+/*
+------ Animacion de menu de cover ------------
+===============================================
+*/
+	$('.item').mouseenter(function() {
+		$(this).velocity({borderBottomWidth : 2, colorAlpha : 1}, {duration : 200});	
+	});
+	
+	$('.item').mouseout(function() {
+		$(this).velocity({borderBottomWidth : 0, colorAlpha : 0.75}, {duration : 0});	
+	});
+
+	$('.drop-cover').mouseenter(function() {
+		$('.drop-menu-cover').velocity({opacity : 1}, {duration : 200});	
+	});
+
+	$('.drop-menu-cover').mouseleave(function() {
+		$(this).velocity({opacity : 0}, {duration : 0});	
+	});
 	
   });//fin
+
+
 
 
 
